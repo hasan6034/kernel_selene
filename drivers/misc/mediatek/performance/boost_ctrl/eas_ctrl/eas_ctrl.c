47040792c2b11ca132dade023096d0eefbe08f7b
fukiame <fukiame@proton.me>
2024-08-02 00:57:35 +0700

    Revert "drivers/misc: mediatek: guard perfmgr_trace_*"
    This reverts commit bb2ccb6ad8cf4f48dd5167a9347dd7587d8ce85a.

--- drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
@@ -302,9 +302,7 @@ int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 
 	if (cgroup_idx >= NR_CGROUP || cgroup_idx < 0) {
 		pr_debug("cgroup_idx:%d, error\n", cgroup_idx);
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("cpu_ctrl", "cgroup_idx >= NR_CGROUP\n");
-#endif
 		return -1;
 	}
 
@@ -321,9 +319,7 @@ int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 
 	/*ptr return error EIO:I/O error */
 	if (len < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("cpu_ctrl", "return -EIO 1\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
@@ -349,9 +345,7 @@ int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 	len += snprintf(msg + len, sizeof(msg) - len, "{%d} ", final_boost);
 	/*ptr return error EIO:I/O error */
 	if (len < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("cpu_ctrl", "return -EIO 2\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
@@ -359,9 +353,7 @@ int update_eas_boost_value(int kicker, int cgroup_idx, int value)
 			policy_mask[cgroup_idx]);
 
 	if (len1 < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("cpu_ctrl", "return -EIO 3\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
@@ -400,9 +392,7 @@ int update_eas_uclamp_min(int kicker, int cgroup_idx, int value)
 
 	if (cgroup_idx >= NR_CGROUP || cgroup_idx < 0) {
 		pr_debug(" cgroup_idx:%d, error\n", cgroup_idx);
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("uclamp_min", "cgroup_idx >= NR_CGROUP\n");
-#endif
 		return -1;
 	}
 
@@ -419,9 +409,7 @@ int update_eas_uclamp_min(int kicker, int cgroup_idx, int value)
 
 	/* ptr return error EIO:I/O error */
 	if (len < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("uclamp_min", "return -EIO 1\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
@@ -444,9 +432,7 @@ int update_eas_uclamp_min(int kicker, int cgroup_idx, int value)
 
 	/*ptr return error EIO:I/O error */
 	if (len < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("uclamp_min", "return -EIO 2\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
@@ -455,9 +441,7 @@ int update_eas_uclamp_min(int kicker, int cgroup_idx, int value)
 			uclamp_policy_mask[cgroup_idx]);
 
 	if (len1 < 0) {
-#ifdef CONFIG_TRACING
 		perfmgr_trace_printk("uclamp_min", "return -EIO 3\n");
-#endif
 		mutex_unlock(&boost_eas);
 		return -EIO;
 	}
