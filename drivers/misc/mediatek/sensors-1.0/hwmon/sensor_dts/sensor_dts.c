18e8368147ecfdf67096b92032e5a60d9df76783
fukiame <fukiame@proton.me>
2024-08-09 08:50:02 +0700

    drivers/misc: mediatek: sensors-1.0: drop gyroscope and accelgyro entirely
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/sensors-1.0/hwmon/sensor_dts/sensor_dts.c
+++ drivers/misc/mediatek/sensors-1.0/hwmon/sensor_dts/sensor_dts.c
@@ -20,11 +20,8 @@
 #define pr_fmt(fmt) "[Sensor dts] " fmt
 
 #include <cust_acc.h>
-#include <cust_accelgyro.h>
 #include <cust_alsps.h>
 #include <cust_baro.h>
-#include <cust_gyro.h>
-//#include <cust_hmdy.h>
 #include <cust_mag.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
@@ -249,68 +246,6 @@ int get_mag_dts_func(struct device_node *node, struct mag_hw *hw)
 	return 0;
 }
 
-int get_gyro_dts_func(struct device_node *node, struct gyro_hw *hw)
-{
-	int i, ret;
-	u32 i2c_num[] = {0};
-	u32 i2c_addr[C_CUST_I2C_ADDR_NUM] = {0};
-	u32 direction[] = {0};
-	u32 power_id[] = {0};
-	u32 power_vol[] = {0};
-	u32 firlen[] = {0};
-	u32 is_batch_supported[] = {0};
-
-	pr_debug("Device Tree get gyro info!\n");
-	if (node) {
-		ret = of_property_read_u32_array(node, "i2c_num", i2c_num,
-						 ARRAY_SIZE(i2c_num));
-		if (ret == 0)
-			hw->i2c_num = i2c_num[0];
-
-		ret = of_property_read_u32_array(node, "i2c_addr", i2c_addr,
-						 ARRAY_SIZE(i2c_addr));
-		if (ret == 0) {
-			for (i = 0; i < GYRO_CUST_I2C_ADDR_NUM; i++)
-				hw->i2c_addr[i] = i2c_addr[i];
-		}
-
-		ret = of_property_read_u32_array(node, "direction", direction,
-						 ARRAY_SIZE(direction));
-		if (ret == 0)
-			hw->direction = direction[0];
-
-		ret = of_property_read_u32_array(node, "power_id", power_id,
-						 ARRAY_SIZE(power_id));
-		if (ret == 0) {
-			if (power_id[0] == 0xffff)
-				hw->power_id = -1;
-			else
-				hw->power_id = power_id[0];
-		}
-
-		ret = of_property_read_u32_array(node, "power_vol", power_vol,
-						 ARRAY_SIZE(power_vol));
-		if (ret == 0)
-			hw->power_vol = power_vol[0];
-
-		ret = of_property_read_u32_array(node, "firlen", firlen,
-						 ARRAY_SIZE(firlen));
-		if (ret == 0)
-			hw->firlen = firlen[0];
-
-		ret = of_property_read_u32_array(
-			node, "is_batch_supported", is_batch_supported,
-			ARRAY_SIZE(is_batch_supported));
-		if (ret == 0)
-			hw->is_batch_supported = is_batch_supported[0];
-	} else {
-		pr_err(
-			"Device Tree: can not find gyro node!. Go to use old cust info\n");
-		return -1;
-	}
-	return 0;
-}
-
 int get_baro_dts_func(struct device_node *node, struct baro_hw *hw)
 {
 	int i, ret;
@@ -333,7 +268,7 @@ int get_baro_dts_func(struct device_node *node, struct baro_hw *hw)
 		ret = of_property_read_u32_array(node, "i2c_addr", i2c_addr,
 						 ARRAY_SIZE(i2c_addr));
 		if (ret == 0) {
-			for (i = 0; i < GYRO_CUST_I2C_ADDR_NUM; i++)
+			for (i = 0; i < 2; i++)
 				hw->i2c_addr[i] = i2c_addr[i];
 		}
 
@@ -371,58 +306,5 @@ int get_baro_dts_func(struct device_node *node, struct baro_hw *hw)
 			"Device Tree: can not find gyro node!. Go to use old cust info\n");
 		return -1;
 	}
-	return 0;
-}
-
-int get_accelgyro_dts_func(struct device_node *node, struct accelgyro_hw *hw)
-{
-	int ret;
-
-	u32 direction[] = {0};
-	u32 accel_firlen[] = {0};
-	u32 gyro_firlen[] = {0};
-	u32 accel_is_batch_supported[] = {0};
-	u32 gyro_is_batch_supported[] = {0};
-
-	pr_debug("Device Tree get accel info!\n");
-	if (node) {
-		ret = of_property_read_u32_array(node, "direction", direction,
-						 ARRAY_SIZE(direction));
-		if (ret == 0)
-			hw->direction = direction[0];
-
-		ret = of_property_read_u32_array(node, "accel_firlen",
-						 accel_firlen,
-						 ARRAY_SIZE(accel_firlen));
-		if (ret == 0)
-			hw->accel_firlen = accel_firlen[0];
-
-		ret = of_property_read_u32_array(
-			node, "accel_is_batch_supported",
-			accel_is_batch_supported,
-			ARRAY_SIZE(accel_is_batch_supported));
-		if (ret == 0)
-			hw->accel_is_batch_supported =
-				accel_is_batch_supported[0];
-
-		ret = of_property_read_u32_array(node, "gyro_firlen",
-						 gyro_firlen,
-						 ARRAY_SIZE(gyro_firlen));
-		if (ret == 0)
-			hw->gyro_firlen = gyro_firlen[0];
-
-		ret = of_property_read_u32_array(
-			node, "gyro_is_batch_supported",
-			gyro_is_batch_supported,
-			ARRAY_SIZE(gyro_is_batch_supported));
-		if (ret == 0)
-			hw->gyro_is_batch_supported =
-				gyro_is_batch_supported[0];
-	} else {
-		pr_err(
-			"Device Tree: can not find accel node!. Go to use old cust info\n");
-		return -1;
-	}
-
 	return 0;
 }
